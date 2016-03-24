1d121ab045ce3af30cea3b85ab547b6c718161e8
Ben Hong <bencodezen@gmail.com>
2016-03-24 11:05:11 -0400

    Use props to display content

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -5,18 +5,20 @@ var MyTitle = React.createClass({
   render () {
     return (
       div(null,
-        h1(null, 'Check out my first class component!')
+        h1(null, this.props.title)
       )
     )
   }
 })
 
 var MyTitleFact = React.createFactory(MyTitle)
+var ce = React.createElement
 
 var MyFirstComponent = (
   div(null,
-    MyTitleFact(null),
-    React.createElement(MyTitle, null)
+    MyTitleFact({title: 'Props are great!'}),
+    React.createElement(MyTitle, {title: 'Use props everywhere!'}),
+    ce(MyTitle, {title: 'Props are the best!'})
   )
 )
 
