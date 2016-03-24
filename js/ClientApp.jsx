4971c750258ce75d8141412620be4753a74114aa
Ben Hong <bencodezen@gmail.com>
2016-03-24 14:53:11 -0400

    Add Search component with data.json

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -2,10 +2,12 @@ const React = require('react')
 const ReactDOM = require('react-dom')
 const { Router, Route, hashHistory } = require('react-router')
 const Landing = require('./Landing')
+const Search = require('./Search')
 
 const App = () => (
   <Router history={hashHistory}>
     <Route path='/' component={Landing} />
+    <Route path='/search' component={Search} />
   </Router>
 )
 
